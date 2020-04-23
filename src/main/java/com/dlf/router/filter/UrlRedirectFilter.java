30d441f26bcbe0217a89889c9f4874442ef7b912
sylphdlf@163.com <dlf@1989128>
2020-04-23 16:27:02 +0800

    20200423

--- src/main/java/com/dlf/router/filter/UrlRedirectFilter.java
+++ src/main/java/com/dlf/router/filter/UrlRedirectFilter.java
@@ -6,9 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class UrlRedirectFilter extends ZuulFilter {
