5c02b06a4df916a8a7068516df1b0347a211058d
Someone <oncwnuOIzH_4tT_FuTFpX7g3Kj3U@git.weixin.qq.com>
2020-05-19 23:35:30 +0800

    ～～～

--- src/main/java/com/dlf/Application.java
+++ src/main/java/com/dlf/Application.java
@@ -3,9 +3,13 @@ package com.dlf;
 import com.dlf.router.filter.UrlRedirectFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
 
+import javax.servlet.MultipartConfigElement;
 
 
 /**
@@ -24,13 +28,13 @@ public class Application {
         return new UrlRedirectFilter();
     }
 
-//    @Bean
-//    public MultipartConfigElement multipartConfigElement() {
-//        MultipartConfigFactory factory = new MultipartConfigFactory();
-//        //允许上传的文件最大值
-//        factory.setMaxFileSize("2MB"); //KB,MB
-//        /// 设置总上传数据总大小
-//        factory.setMaxRequestSize("2MB");
-//        return factory.createMultipartConfig();
-//    }
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //允许上传的文件最大值
+        factory.setMaxFileSize(DataSize.of(2, DataUnit.MEGABYTES));
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize(DataSize.of(2, DataUnit.MEGABYTES));
+        return factory.createMultipartConfig();
+    }
 }
